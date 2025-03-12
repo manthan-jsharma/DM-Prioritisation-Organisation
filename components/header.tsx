b21d2e0cca102df88f20cd252e16aa0731154006
manthan <manthan.jsharma@gmail.com>
2025-03-12 16:25:57 +0530

    added ui components

---
+++ components/header.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { signOut, useSession } from "next-auth/react";
+import { Bell, LogOut, Settings } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
