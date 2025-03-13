4d5a681825abc2d7f393f75315bde0c1da972e84
manthan <manthan.jsharma@gmail.com>
2025-03-13 18:11:23 +0530

    implemented session type

--- app/actions.tsx
+++ app/actions.tsx
@@ -1,14 +1,14 @@
 "use server";
 
 import { getServerSession } from "next-auth/next";
+
 import { fetchEmails, type Email } from "@/lib/gmail";
 
 export async function getEmails(): Promise<Email[]> {
   const session = await getServerSession();
-
   if (!session?.accessToken) {
     throw new Error("Not authenticated");
   }
 
-  return fetchEmails(session.accessToken);
+  return fetchEmails(accessToken);
 }
