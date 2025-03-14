6c52ac33dc0451834951c7b39d1b8a3e8f76f17e
manthan <manthan.jsharma@gmail.com>
2025-03-14 13:20:03 +0530

    wrapped up with nextauth provider

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,9 +1,10 @@
+"use client";
 import type React from "react";
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 import Header from "@/components/header";
 import Sidebar from "@/components/sidebar";
-
+import { NextAuthProvider } from "@/components/next-auth-provider";
 export default async function AuthenticatedLayout({
   children,
 }: {
@@ -16,12 +17,14 @@ export default async function AuthenticatedLayout({
   }
 
   return (
-    <div className="flex h-screen bg-gray-50">
-      <Sidebar />
-      <div className="flex flex-col flex-1 overflow-hidden">
-        <Header />
-        <main className="flex-1 overflow-y-auto p-4">{children}</main>
+    <NextAuthProvider session={session}>
+      <div className="flex h-screen bg-gray-50">
+        <Sidebar />
+        <div className="flex flex-col flex-1 overflow-hidden">
+          <Header />
+          <main className="flex-1 overflow-y-auto p-4">{children}</main>
+        </div>
       </div>
-    </div>
+    </NextAuthProvider>
   );
 }
