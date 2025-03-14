58d82b1386ed5ce1f35597200e120e33b14acb41
manthan <manthan.jsharma@gmail.com>
2025-03-14 19:29:43 +0530

    wrapped up layout function

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -5,15 +5,27 @@ import { redirect } from "next/navigation";
 import Header from "@/components/header";
 import Sidebar from "@/components/sidebar";
 import { NextAuthProvider } from "@/components/next-auth-provider";
+// import { useState, useEffect } from "react";
+
 export default async function AuthenticatedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  //   const [session, setSession] = useState(null);
+
+  //   useEffect(() => {
+  //     const fetchSession = async () => {
+  //       const response = await fetch("/api/session");
+  //       const data = await response.json();
+  //       setSession(data);
+  //     };
+  //     fetchSession();
+  //   }, []);
   const session = await getServerSession();
 
   if (!session) {
-    redirect("/login");
+    redirect("/dashboard");
   }
 
   return (
